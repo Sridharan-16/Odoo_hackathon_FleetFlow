36ba1ad0b32b7120174dcd484d7e4536fecdb8db
Shan070106 <kit27.am51@gmail.com>
2026-02-21 14:32:20 +0530

    validating schema and body

---
+++ backend/src/middlewares/validation.middleware.js
@@ -0,0 +1,17 @@
+const validationMiddleware = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        errors: error.details.map(detail => detail.message)
+      });
+    }
+    
+    next();
+  };
+};
+
+export default validationMiddleware;
